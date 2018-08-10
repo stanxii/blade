403a8a3422106f09378d5897e0119aabf81c5cc9
Andy Pan <panjf2000@gmail.com>
2018-08-10 22:35:07 +0800

    新增

---
+++ 递归分治/qsort.cpp
@@ -0,0 +1,30 @@
+template <class Type>
+void QuickSort(Type a[], int p, int r)
+{
+    if (p < r)
+    {
+        int q = Partition(a, p, r);
+        QuickSort(a, p, q - 1);
+        QuickSort(a, q + 1, r);
+    }
+}
+
+template <class Type>
+int Partition(Type a[], int p, int r)
+{
+    int i = p, j = r + 1;
+    Type x = a[p];
+    while (true)
+    {
+        while (a[++i] < x && i < r);
+        while (a[--j] > x);
+        if (i >= j)
+            break;
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+    a[p] = a[j];
+    a[j] = x;
+    return j;
+}
