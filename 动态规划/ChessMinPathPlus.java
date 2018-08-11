953f9dfa770b55034ae7b1ace590fecdca5ef142
Andy Pan <panjf2000@gmail.com>
2018-08-11 22:32:50 +0800

    动态规划(滚动数组)解决格子取数/走棋盘问题

---
+++ 动态规划/ChessMinPathPlus.java
@@ -0,0 +1,23 @@
+class ChessMinPathPlus {
+    public static int minPath(int[][] chess) {
+        int row = chess.length;
+        int col = chess[0].length;
+        int[] dp = new int[col];
+        dp[0] = chess[0][0];
+        for(int j=1; j<col; j++)
+            dp[j] = dp[j-1] + chess[0][j];
+        for(int i=1; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                if(j == 0)
+                    dp[j] += chess[i][j];
+                else
+                    dp[j] = (dp[j] < dp[j-1] ? dp[j] : dp[j-1]) + chess[i][j];
+            }
+        }
+        return dp[col-1];
+    }
+    public static void main(String[] args) {
+        int[][] chess = {{7,3,4},{8,12,3},{6,9,20}};    
+        System.out.println(minPath(chess));
+    }
+}
