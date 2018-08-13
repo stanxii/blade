c9cf7e0f5edb78f7e27f7809ae3d184cdf65a33c
Andy Pan <panjf2000@gmail.com>
2018-08-13 23:31:48 +0800

    BitSet对非重复数组排序

---
+++ BitSet应用/BitSetSort.java
@@ -0,0 +1,39 @@
+import java.util.BitSet;
+
+class BitSetSort {
+    public static void main(String[] args) {
+        int[] array = new int[] { 99, 23, 7, 22, 232, 6, 64, 3, 2, 1 };
+        sortArray(array);
+    }
+    public static void sortArray(int[] array) {
+		BitSet bitSet = new BitSet(2 << 10);
+		// 虽然可以自动扩容，但尽量在构造时指定估算大小,默认为64
+		System.out.println("BitSet size: " + bitSet.size());
+ 
+		for (int i = 0; i < array.length; i++) {
+			bitSet.set(array[i]);
+		}
+		//剔除重复数字后的元素个数
+		int bitLen=bitSet.cardinality();	
+ 
+		//进行排序，即把bit为true的元素复制到另一个数组
+		int[] orderedArray = new int[bitLen];
+		int k = 0;
+		for (int i = bitSet.nextSetBit(0); i >= 0; i = bitSet.nextSetBit(i + 1)) {
+			orderedArray[k++] = i;
+		}
+ 
+		System.out.println("After ordering: ");
+		for (int i = 0; i < bitLen; i++) {
+			System.out.print(orderedArray[i] + "\t");
+		}
+		
+		System.out.println("iterate over the true bits in a BitSet");
+		//或直接迭代BitSet中bit为true的元素
+		for (int i = bitSet.nextSetBit(0); i >= 0; i = bitSet.nextSetBit(i + 1)) {
+			System.out.print(i+"\t");
+		}
+		System.out.println("---------------------------");
+	}
+
+}
