d9298275e64c4b6c7a0f25a002eb0d72ea7711ff
Andy Pan <panjf2000@gmail.com>
2018-08-17 22:35:33 +0800

    快排修正

--- 排序算法/QuickSort.java
+++ 排序算法/QuickSort.java
@@ -8,8 +8,7 @@ class QuickSort {
     }
 
     public int partition(int[] a, int i, int j) {
-        int b = i;
-        int e = j + 1;
+        int b = i, e = j + 1;
         int x = a[i];
         while (true) {
             while (b < j && a[++b] < x)
@@ -25,6 +24,6 @@ class QuickSort {
         }
         a[i] = a[e];
         a[e] = x;
-        return b;
+        return e;
     }
 }
