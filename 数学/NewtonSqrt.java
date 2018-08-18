40ebfd5466571da60979e32917f2e3507a050354
Andy Pan <panjf2000@gmail.com>
2018-08-18 17:27:24 +0800

    二分法求二次方根对比

--- 数学/NewtonSqrt.java
+++ 数学/NewtonSqrt.java
@@ -25,4 +25,29 @@ class NewtonSqrt {
         System.out.printf("final result = %f\n", guess);
         return guess;
     }
+
+    // 二分法求根
+    public static double DichotomySqrt(double number, double accuracy) {
+        double higher = number;
+        double lower = 0.0;
+        double middle = (lower + higher) / 2;
+        double last_middle = 0.00;
+        int count = 0;
+        if (number < 0) {
+            return Double.NaN;
+        }
+        while (Math.abs(middle - last_middle) > accuracy) {
+            if (middle * middle > number) {
+                higher = middle;
+            } else {
+                lower = middle;
+            }
+            last_middle = middle;
+            middle = (lower + higher) / 2;
+            count++;
+            System.out.printf("Dichotomy try count = %d, guess = %f\n", count, last_middle);
+        }
+        System.out.printf("Dichotomy final result = %f\n", last_middle);
+        return last_middle;
+    }
 }
