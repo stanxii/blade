5fa3aed325091798a8a63834f8e426b4a938380d
Andy Pan <panjf2000@gmail.com>
2018-08-18 11:37:37 +0800

    求链表环长度

---
+++ 链表/ListRing.java
@@ -0,0 +1,36 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class ListRing {
+    public int getRingLength(ListNode head) {
+        if (head == null || head.next == null) {
+            return 0;
+        }
+        ListNode slow = head, fast = head.next.next;
+        while (fast != null && fast.next != null) {
+            if (slow == fast) {
+                return getLength(fast);
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return 0;
+    }
+
+    public int getLength(ListNode l) {
+        int length = 1;
+        ListNode cur = l;
+        while (cur.next != l) {
+            cur = cur.next;
+            length++;
+        }
+        return length;
+    }
+}
