2da50051a73f08b00eb9b0fe7cd1d9e960963a3b
Andy Pan <panjf2000@gmail.com>
2018-09-16 17:02:56 +0800

    [LeetCode] Longest Consecutive Sequence 求最长连续序列

---
+++ 哈希/LCS.java
@@ -0,0 +1,18 @@
+public class LCS {
+    public int longestConsecutive(int[] nums) {
+        int res = 0;
+        Map<Integer, Integer> m = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            if (m.containsKey(num))
+                continue;
+            int left = m.containsKey(num - 1) ? m.get(num - 1) : 0;
+            int right = m.containsKey(num + 1) ? m.get(num + 1) : 0;
+            int sum = left + right + 1;
+            m.put(num, sum);
+            res = Math.max(res, sum);
+            m.put(num - left, sum);
+            m.put(num + right, sum);
+        }
+        return res;
+    }
+}
