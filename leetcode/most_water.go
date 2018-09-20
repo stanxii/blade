2dc5a7ae212fb949f9206d4458b2b3471109cecf
Andy Pan <panjf2000@gmail.com>
2018-09-20 23:01:11 +0800

    LeetCode 11 — Container With Most Water(盛最多水的容器)

--- leetcode/most_water.go
+++ leetcode/most_water.go
@@ -3,27 +3,27 @@ package main
 import "fmt"
 
 func maxArea(height []int) int {
-    i, j, s, ms := 0, len(height) - 1, 0, 0
-    for i < j {
-        if height[i] < height[j] {
-            s = height[i] * (j - i)
-            i++
-        } else {
-            s = height[j] * (j - i)
-            j--
-        }
-        ms = max(s, ms)
-    }
-    return ms
+	i, j, s, ms := 0, len(height)-1, 0, 0
+	for i < j {
+		if height[i] < height[j] {
+			s = height[i] * (j - i)
+			i++
+		} else {
+			s = height[j] * (j - i)
+			j--
+		}
+		ms = max(s, ms)
+	}
+	return ms
 }
-func max (a, b int) int{
-    if a < b {
-        return b
-    }
-    return a
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
 }
 
-func main(){
-	arr := {7,3,8,12,5}
+func main() {
+	arr := []int{7, 3, 8, 12, 5}
 	fmt.Println(maxArea(arr))
 }
