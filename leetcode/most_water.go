7595105aee8781388b2192625d74686ccb57b741
Andy Pan <panjf2000@gmail.com>
2018-09-20 22:46:50 +0800

    LeetCode 11 — Container With Most Water(盛最多水的容器)
    golang实现

---
+++ leetcode/most_water.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func maxArea(height []int) int {
+    i, j, s, ms := 0, len(height) - 1, 0, 0
+    for i < j {
+        if height[i] < height[j] {
+            s = height[i] * (j - i)
+            i++
+        } else {
+            s = height[j] * (j - i)
+            j--
+        }
+        ms = max(s, ms)
+    }
+    return ms
+}
+func max (a, b int) int{
+    if a < b {
+        return b
+    }
+    return a
+}
+
+func main(){
+	arr := {7,3,8,12,5}
+	fmt.Println(maxArea(arr))
+}
