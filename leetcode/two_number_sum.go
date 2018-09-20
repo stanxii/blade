f68dcfa25b05688cbd6adcfdc8b92109c4b3d29d
Andy Pan <panjf2000@gmail.com>
2018-09-20 22:50:08 +0800

    两数之和

---
+++ leetcode/two_number_sum.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+func twoSum(nums []int, target int) []int {
+    m := make(map[int]int)
+    for i, v := range nums {
+        temp := target - v
+        if value, ok := m[temp]; ok {
+            result := []int {value, i}
+            return result
+        }
+        m[v] = i
+    }
+    return nil
+}
+func main(){
+	arr := {3,5,1,9,18,7,11}
+	fmt.Println(twoSum(arr, 18))
+}
