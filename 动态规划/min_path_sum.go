e28f1522c875a8e26664a3283f75138ebe6802f9
Andy Pan <panjf2000@gmail.com>
2019-03-10 23:03:09 +0800

    棋盘最小路径和，golang实现

---
+++ 动态规划/min_path_sum.go
@@ -0,0 +1,24 @@
+package main
+
+func minPathSum(grid [][]int) int {
+	row := len(grid)
+	col := len(grid[0])
+	dp := make([]int, col)
+	dp[0] = grid[0][0]
+	for j := 1; j < col; j++ {
+		dp[j] = dp[j-1] + grid[0][j]
+	}
+	for i := 1; i < row; i++ {
+		for j := 0; j < col; j++ {
+			if j == 0 {
+				dp[j] += grid[i][j]
+			} else if dp[j] < dp[j-1] {
+				dp[j] = dp[j] + grid[i][j]
+			} else if dp[j] >= dp[j-1] {
+				dp[j] = dp[j-1] + grid[i][j]
+			}
+		}
+
+	}
+	return dp[col-1]
+}
